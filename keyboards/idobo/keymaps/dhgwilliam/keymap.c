cda779f03b82cbec80d5bcf795b3fbaaad7944cf
David Gwilliam <david.gwilliam@skyward.io>
2020-10-07 10:53:36 -0700

    updates to idobo layout

--- keyboards/idobo/keymaps/dhgwilliam/keymap.c
+++ keyboards/idobo/keymaps/dhgwilliam/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT_ortho_5x15(
       ALL_T(KC_GRV)  , KC_1    , KC_2          , KC_3    , KC_4    , KC_5   , KC_PGDN     , KC_UP   , KC_PGUP     , KC_6   , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS         ,
       KC_TAB         , KC_Q    , KC_W          , KC_E    , KC_R    , KC_T   , KC_LEFT     , KC_DOWN , KC_RGHT     , KC_Y   , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC         ,
-      LCTL_T(KC_ESC) , KC_A    , KC_S          , KC_D    , KC_F    , KC_G   , KC_LGUI     , KC_MPLY , KC_RGUI     , KC_H   , KC_J    , KC_K    , KC_L    , KC_SCLN , RCTL_T(KC_QUOT) ,
+      LCTL_T(KC_ESC) , KC_A    , KC_S          , KC_D    , KC_F    , KC_G   , KC_LGUI     , KC_MPLY , KC_RGUI     , KC_H   , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,
       KC_LSFT        , KC_Z    , KC_X          , KC_C    , KC_V    , KC_B   , KC_LALT     , KC_HOME , KC_RALT     , KC_N   , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , RSFT_T(KC_ENT)  ,
       KC_LEAD        , KC_CAPS , LALT(KC_LSFT) , KC_LALT , KC_LGUI , KC_SPC , MO(_SYMBOL) , KC_END  , MO(_NUMBER) , KC_SPC , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , MO(_META)
       ),
@@ -36,8 +36,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_META] = LAYOUT_ortho_5x15(
       KC_NO , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , RESET   ,
       KC_NO , KC_NO , KC_NO , KC_NO   , KC_MS_U , KC_NO   , KC_NO   , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO   ,
-      KC_NO , KC_NO , KC_NO , KC_MS_L , KC_MS_D , KC_MS_R , KC_NO   , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_MPLY ,
+      KC_NO , KC_NO , KC_NO , KC_MS_L , KC_MS_D , KC_MS_R , KC_NO   , KC_NO , KC_NO   , KC_NO   , CMB_TOG , KC_NO   , KC_NO   , KC_NO , KC_MPLY ,
       KC_NO , KC_NO , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_MRWD , KC_MFFD , KC_NO , KC_NO   ,
       KC_NO , KC_NO , KC_NO , KC_MS_L , KC_MS_R , KC_NO   , KC_TRNS , KC_NO , KC_TRNS , KC_WBAK , KC_VOLD , KC_VOLU , KC_MUTE , KC_NO , KC_NO
       )
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case RCTL_T(KC_QUOT):
+            return TAPPING_TERM + 1250;
+        default:
+            return TAPPING_TERM;
+    }
+}
